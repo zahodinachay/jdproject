87dcd73cf3b76ae452408c2756b1724968e4d318
zahodinachay <zahodinachay@gmail.com>
2020-03-26 10:18:34 +0300

    css fixed

--- django_project_boilerplate/core/models.py
+++ django_project_boilerplate/core/models.py
@@ -3,7 +3,8 @@ from django.db import models
 
 class Item(models.Model):
     title = models.CharField(max_length=100)
-
+    price = models.FloatField()
+    
     def __str__(self):
         return self.title
 
